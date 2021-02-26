f59b258c7bfbc9fc2354e3f46e888d2bf26659e3
osiris79127 <79582946+osiris79127@users.noreply.github.com>
2021-02-26 22:04:34 +0800

    Add files via upload

---
+++ 0226-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 18:47:05 2021
+
+@author: USER
+"""
+
+for i in range(1,5):
+    for a in range(i):
+        print(a,end='')
+    print()
+print('程式執行完畢')
