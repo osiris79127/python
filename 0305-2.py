cd42be0fd5a646cb578af380ec2b61195a042698
osiris79127 <79582946+osiris79127@users.noreply.github.com>
2021-03-05 22:07:26 +0800

    Add files via upload

---
+++ 0305-2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 20:16:18 2021
+
+@author: USER
+"""
+
+import random
+#1
+b=list()
+c=0
+while c<6:
+    a=random.randint(1,49)
+    if b.count(a)==0:
+        b.append(a)
+        c+=1
+print(b)
+#2
+d=list(range(1,50,1))
+h=list()
+e=0
+while e<6:
+    f=random.randint(1,49)
+    if h.count(f)==0:
+        h.append(f)
+        e+=1
+print(h)
